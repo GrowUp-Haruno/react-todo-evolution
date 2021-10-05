3aacc6dd9cdbb8303416fbcd73cb73ae723583eb
Daisuke Wada <74647388+GrowUp-Haruno@users.noreply.github.com>
2021-10-05 23:25:20 +0900

    ChakraProvider追加

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
-import { Grid, GridItem } from '@chakra-ui/react'
+import { ChakraProvider, Grid, GridItem } from '@chakra-ui/react'
 import { FC, memo } from 'react'
 import Header from './components/layouts/Header'
 
 const App: FC = memo(() => (
-  <Grid h="100vh" w="100vw" templateRows="repeat(12, 1fr)" templateColumns="repeat(12, 1fr)" gap={0}>
-    <Header />
-    <GridItem rowStart={2} rowEnd={12} colStart={1} colEnd={13} bg="white" />
-    <GridItem rowStart={12} rowEnd={12} colStart={1} colEnd={13} bg="lightgray" />
-  </Grid>
+  <ChakraProvider>
+    <Grid h="100vh" w="100vw" templateRows="repeat(12, 1fr)" templateColumns="repeat(12, 1fr)" gap={0}>
+      <Header />
+      <GridItem rowStart={2} rowEnd={12} colStart={1} colEnd={13} bg="white" />
+      <GridItem rowStart={12} rowEnd={12} colStart={1} colEnd={13} bg="lightgray" />
+    </Grid>
+  </ChakraProvider>
 ))
 
 App.displayName = 'App'
