23e50ec2b7c24f343462e7b3cfaf28c3e1156935
Daisuke Wada <74647388+GrowUp-Haruno@users.noreply.github.com>
2021-10-08 23:32:52 +0900

    時計動作確認

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,28 @@
 import { ChakraProvider, Grid, GridItem } from '@chakra-ui/react'
-import { FC, memo } from 'react'
+import { FC, memo, useState } from 'react'
 import Header from './components/layouts/Header'
 
-const App: FC = memo(() => (
-  <ChakraProvider>
-    <Grid h="100vh" w="100vw" templateRows="repeat(12, 1fr)" templateColumns="repeat(12, 1fr)" gap={0}>
-      <Header />
-      <GridItem rowStart={2} rowEnd={12} colStart={1} colEnd={13} bg="white" />
-      <GridItem rowStart={12} rowEnd={12} colStart={1} colEnd={13} bg="lightgray" />
-    </Grid>
-  </ChakraProvider>
-))
+const App: FC = memo(() => {
+  const [time, setTime] = useState<string>('')
+  setInterval(() => {
+    const nowTime = new Date()
+    const nowHour = nowTime.getHours()
+    const nowMint = nowTime.getMinutes()
+    const nowSec = nowTime.getSeconds()
+    setTime(`現在時刻 ${nowHour}:${nowMint}:${nowSec}`)
+  }, 1000)
+  return (
+    <ChakraProvider>
+      <Grid h="100vh" w="100vw" templateRows="repeat(12, 1fr)" templateColumns="repeat(12, 1fr)" gap={0}>
+        <Header />
+        <GridItem rowStart={2} rowEnd={12} colStart={1} colEnd={13} bg="white">
+          <p>{time}</p>
+        </GridItem>
+        <GridItem rowStart={12} rowEnd={12} colStart={1} colEnd={13} bg="lightgray" />
+      </Grid>
+    </ChakraProvider>
+  )
+})
 
 App.displayName = 'App'
 
