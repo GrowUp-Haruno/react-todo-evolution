612a3110e5458aa98760276fc7e9fe9fbdbc84b5
Daisuke Wada <74647388+GrowUp-Haruno@users.noreply.github.com>
2021-10-09 22:40:50 +0900

    時計機能をカスタムフック化

---
+++ src/hooks/useClock.tsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+
+type useClockType = () => {
+  time: string
+}
+
+const useClock:useClockType = () => {
+  const [time, setTime] = useState<string>('')
+  setInterval(() => {
+    const nowTime = new Date()
+    const nowHour = nowTime.getHours()
+    const nowMint = nowTime.getMinutes()
+    const nowSec = nowTime.getSeconds()
+    setTime(`現在時刻 ${nowHour}:${nowMint}:${nowSec}`)
+  }, 1000)
+
+  return { time }
+}
+
+export default useClock
